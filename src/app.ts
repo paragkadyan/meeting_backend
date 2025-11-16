982dda7abe31320a2fecdd2b779d4122e4b6a6e4
Parag Kadyan <kadyanparag@gmail.com>
2025-11-17 01:40:36 +0530

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send(`running on port ${process.env.PORT || 3000}`);
+});
+
+export default app;
